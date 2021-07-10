f035d97c4c752cc06d25921ee8e0c21e5792dab9
Almudena Garcia <liberamenso10000@gmail.com>
2021-07-11 01:13:28 +0200

    upload new images

--- uca/mariadb_uca/sql/uca.sql
+++ uca/mariadb_uca/sql/uca.sql
@@ -45,3 +45,16 @@ CREATE TABLE IF NOT EXISTS uca.matriculados (
     REFERENCES uca.alumnos (id)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION);
+    
+INSERT INTO uca.alumnos(cod, nombre, apellidos, password, dni, letra, correo)
+        VALUES('juan.perez', 'Juan', 'Perez Martin', 'user1', '56128914', 'K', 'juan.perez@uca.es');
+
+INSERT INTO uca.alumnos(cod, nombre, apellidos, password, dni, letra, correo)
+        VALUES('alvaro.gomez', 'Alvaro', 'Gomez Martin', 'user2' , '23081274', 'L', 'alvaro.gomez@uca.es');
+        
+INSERT INTO uca.matriculados(id_alumno, status) VALUES (1, 'a');
+INSERT INTO uca.matriculados(id_alumno, status) VALUES (2, 'a');
+
+INSERT INTO uca.asignaturas(acod, cod) VALUES('5021339', '5021339');
+INSERT INTO uca.asignaturas(acod, cod) VALUES('5021340', '5021340');
+INSERT INTO uca.asignaturas(acod, cod) VALUES('5021341', '5021341');
--- upo/mariadb_upo/sql/upo.sql
+++ upo/mariadb_upo/sql/upo.sql
@@ -45,4 +45,15 @@ CREATE TABLE IF NOT EXISTS upo.matriculados (
     ON DELETE NO ACTION
     ON UPDATE NO ACTION);
     
+INSERT INTO upo.alumnos(identificador, nombre, ape1, ape2, password, dni, correo)
+        VALUES('sara.marquez', 'Sara', 'Marquez', 'Lopez', 'user3', '12563108J', 'sara.marquez@upo.es');
 
+INSERT INTO upo.alumnos(identificador, nombre, ape1, ape2, password, dni, correo)
+        VALUES('marta.santos', 'Marta', 'Santos', 'Garcia', 'user4', '10437984H', 'marta.santos@upo.es');
+        
+INSERT INTO upo.matriculados(id_alumno, enabled) VALUES (1, 1);
+INSERT INTO upo.matriculados(id_alumno, enabled) VALUES (2, 1);
+
+INSERT INTO upo.asignaturas(acod, cod) VALUES('5021336', '5021336');
+INSERT INTO upo.asignaturas(acod, cod) VALUES('5021337', '5021337');
+INSERT INTO upo.asignaturas(acod, cod) VALUES('5021338', '5021338');

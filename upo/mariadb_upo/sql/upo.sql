07874bfc36d634d2af9563798b2df8d05a0dc1b7
Almudena Garcia <liberamenso10000@gmail.com>
2021-07-12 15:45:47 +0200

    fix asignaturas and matriculados insertion

--- upo/mariadb_upo/sql/upo.sql
+++ upo/mariadb_upo/sql/upo.sql
@@ -51,9 +51,15 @@ INSERT INTO upo.alumnos(identificador, nombre, ape1, ape2, password, dni, correo
 INSERT INTO upo.alumnos(identificador, nombre, ape1, ape2, password, dni, correo)
         VALUES('marta.santos', 'Marta', 'Santos', 'Garcia', 'user4', '10437984H', 'marta.santos@upo.es');
         
-INSERT INTO upo.matriculados(id_alumno, enabled) VALUES (1, 1);
-INSERT INTO upo.matriculados(id_alumno, enabled) VALUES (2, 1);
+INSERT INTO upo.asignaturas(cod, acod) VALUES('5021336', '2021-22');
+INSERT INTO upo.asignaturas(cod, acod) VALUES('5021337', '2021-22');
+INSERT INTO upo.asignaturas(cod, acod) VALUES('5021338', '2021-22');
 
-INSERT INTO upo.asignaturas(acod, cod) VALUES('5021336', '5021336');
-INSERT INTO upo.asignaturas(acod, cod) VALUES('5021337', '5021337');
-INSERT INTO upo.asignaturas(acod, cod) VALUES('5021338', '5021338');
+INSERT INTO upo.matriculados(id_alumno, id_asignatura, enabled) VALUES (1, 1, 1);
+INSERT INTO upo.matriculados(id_alumno, id_asignatura, enabled) VALUES (2, 1, 1);
+
+INSERT INTO upo.matriculados(id_alumno, id_asignatura, enabled) VALUES (1, 2, 1);
+INSERT INTO upo.matriculados(id_alumno, id_asignatura, enabled) VALUES (2, 2, 1);
+
+INSERT INTO upo.matriculados(id_alumno, id_asignatura, enabled) VALUES (1, 3, 1);
+INSERT INTO upo.matriculados(id_alumno, id_asignatura, enabled) VALUES (2, 3, 1);
